81dad9791d2161eb64270ae16e4ad02aa0f0a318
sihwan0Lee <stay136@gmail.com>
2021-02-07 18:10:31 +0900

    complete ordermake view when client does not have own address, and add order del view

--- eatexpress/order/urls.py
+++ eatexpress/order/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import OrderMake, OrderDel
+from .views import OrderMake, OrderDel, OrderListView
 urlpatterns = [
 
     path('/mk/<int:order_id>/<int:product_id>', OrderMake.as_view()),
-    path('/del/<int:order_id>', OrderDel.as_view())
+    path('/del/<int:order_id>', OrderDel.as_view()),
+    path('/list/<int:order_id>', OrderListView.as_view())
 ]
--- eatexpress/order/views.py
+++ eatexpress/order/views.py
@@ -32,7 +32,8 @@ class OrderMake(View):
                             new_order.save()
                             return JsonResponse({'message': "장바구니에 담겼습니다"}, status=200)
                         return JsonResponse({'message': '해당 상품은 존재하지 않습니다'}, status=400)
-                return JsonResponse({'message': '주소를 변경해주세요'}, status=400)
+
+                return JsonResponse({'message': '주소를 등록해주세요'}, status=400)
 
             else:
                 new_order = Order(
@@ -40,7 +41,6 @@ class OrderMake(View):
                 )
                 new_order.save()
                 return JsonResponse({'message': '장바구니가 생성됬습니다'}, status=200)
-            # return JsonResponse({'message': '장바구니를 먼저 생성하세요'}, status=400)
 
         except KeyError:
             return JsonResponse({'error': '올바르지 않은 키 값'}, status=400)
@@ -48,15 +48,33 @@ class OrderMake(View):
 
 class OrderListView(View):
     @member_verification
-    def get(self, request, user_id):
-        user = request.user
+    def get(self, request, order_id):
         user_s_id = request.userid
-        # if User.objects.filter(username=user).exists():
+        if Order.objects.filter(username_id=user_s_id).exists():  # 검증된 회원의 장바구니가 존재한다면
+            username_id = Order.objects.get(username_id=user_s_id)
+            if Order.objects.filter(id=order_id).exists():
+                username_id_intable = Order.objects.get(
+                    id=order_id).username_id
+                if username_id == username_id_intable:
+                    #   infos = OrderProduct.objects.filter(
+                    #      order_id=order_id).values()
+                    # orderinfo = [
+                    #  {
+
+                    #     'order_id': info.order_id,
+                    #    'product_id': info.product_id,
+                    #   'quantity': info.quantity
+                    # }for info in infos
+                    # ]
+                    return JsonResponse({'orderinfo': "일치"}, status=200)
+                return JsonResponse({'error': "본인과 일치하는 장바구니가 없습니다"}, status=400)
+            return JsonResponse({'error': "장바구니를 확인해주세요"}, status=400)
+        return JsonResponse({'error': "검증되지 않음."}, status=400)
         #   if user.level == 'admin':
 
 
 class OrderDel(View):
-    @member_verification
+    @ member_verification
     def post(self, request, order_id):
         user_s_id = request.userid
         # 인증된 유저아이디와 오더테이블의 유저네임 아이디가 같을때, 내 주문정보만을 가져올수 있게됌.
