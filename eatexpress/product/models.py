54492d3807c0cfc2b0c5b705ac9f8565be2b3432
sihwan0Lee <stay136@gmail.com>
2021-05-16 23:48:57 +0900

    edit model

--- eatexpress/product/models.py
+++ eatexpress/product/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=200, verbose_name='상품명')
-    manufacturer = models.CharField(max_length=100, verbose_name='제조사')
+    name = models.CharField(max_length=50, verbose_name='상품명')
+    manufacturer = models.CharField(max_length=20, verbose_name='제조사')
     stock = models.IntegerField(verbose_name='재고수량')
     unit_price = models.IntegerField(verbose_name='단가')
     expiration_date = models.DateTimeField(
